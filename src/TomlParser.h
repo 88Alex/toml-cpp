90c4f13aef371b54785f41b9ca54493cb6706666
Alex <epicpiano8@gmail.com>
2013-04-14 18:24:42 -0400

    Put each class declaration in separate header file

--- src/TomlParser.h
+++ src/TomlParser.h
@@ -1,55 +1,13 @@
 #ifndef TOMLPARSER
 #define TOMLPARSER
+
 #include <iostream>
 #include <fstream>
 #include <vector>
-
-class TomlElement
-{
-    private:
-        string name;
-        TomlElement parent;//null if its parent is the root
-    public:
-        virtual TomlElement getParent();
-        virtual string getName();
-        virtual string getFullName();
-        //constructor deliberately left out.
-        friend class TomlParser;
-}
-
-class TomlKey : TomlElement
-{
-    private:
-        string type;//All caps, short form i.e. STRING, NUMBER, BOOL, DATE.
-        string value;
-    public:
-        string getType();
-        string getValue();//You can parse it afterwards.
-        TomlKey(string name_, string type_, string value_);
-        friend class TomlParser;
-}
-
-class TomlArray : TomlElement
-{
-    private:
-        vector<TomlElement> elements;
-    public:
-        vector<TomlElement> getArrayElements();
-        TomlArray(string name_, TomlElement parent_, vector<TomlElement> elements_);
-        string getType();//always returns "ARRAY"
-        friend class TomlParser;
-}
-
-class TomlHash : TomlElement
-{
-    private:
-        vector<TomlElement> elements;
-    public:
-        vector<TomlElement> getHashElements();
-        TomlHash(string name_, TomlElement parent_, vector<TomlElement> elements_);
-        string getType();//always returns "HASH"
-        friend class Tomlparser;
-}
+#include "TomlElement.h"
+#include "TomlKey.h"
+#include "TomlArray.h"
+#include "TomlHash.h"
 
 class TomlParser
 {
