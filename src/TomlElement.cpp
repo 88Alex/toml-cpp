1a64f892e88437a1a05cdc0704ce3c0cd85eaf64
Alex <epicpiano8@gmail.com>
2013-04-18 19:26:20 -0300

    Update TomlElement.cpp

--- src/TomlElement.cpp
+++ src/TomlElement.cpp
@@ -1,5 +1,5 @@
 #include "TomlElement.h"
-TomlElement TomlElement::getParent() { return parent; }
+TomlElement* TomlElement::getParent() { return parent; }
 string TomlElement::getName() { return name; }
 string TomlElement::getFullName() { return parent.getName() + "." + name; }
 string TomlElement::getType() { return "ELEMENT"; }
