eca6a0f45ff9331d3b092171ee2bd61ce32051d3
Alex <epicpiano8@gmail.com>
2013-05-07 09:35:12 -0400

    Added code to parse keys

--- src/TomlParser.cpp
+++ src/TomlParser.cpp
@@ -1,5 +1,62 @@
 #include "TomlParser.h"
-#include <fstream>
+static long TomlParser::parseInteger(string text)
+{
+    int index = 0;
+    char currChar = text.at(index);
+    bool isNegative = false;
+    int currNumber;
+    if(currChar == '+')
+    {
+        index++;
+        currChar = text.at(index);
+    }
+    else if(currChar == '-')
+    {
+        isNegative = true;
+        index++;
+        currChar = text.at(index);
+    }
+    for(; index < text.length(); index++, currChar = text.at(index))
+    {
+        if(48 <= currChar && currChar <= 57)
+        {
+            currNumber *= 10;
+            currNumber += (currChar - 48);
+        }
+        else
+        {
+            throw 1; // 1 = Error parsing integer
+        }
+    }
+}
+static bool TomlParser::parseBoolean(string text)
+{
+    if(text == "true") return true;
+    else if(text == "false") return false;
+    else throw 2; // 2 = error parsing boolean
+}
+static double TomlParser::parseDouble(string text)
+{
+    //
+}
+static void TomlParser::clipSpaces(string& text)
+{
+    bool isStrMode = true;//We don't want to clip spaces inside strings
+    int index = 0;
+    char currChar = text.at(index);
+    for(; index < text.length(); index++, currChar = text.at(index))
+    {
+        if(isStrMode) break;
+        else if(currChar = '\'' || currChar = '"') isStrMode = true;
+        else if(currChar = ' ' || currChar = '\t')
+        {
+            front = text.substr(0, index);
+            back = text.substr(index + 1);
+            front.append(back);
+            text = front;
+        }
+    }
+}
 static void TomlParser::setFile(string name)
 {
     fstream newStream(name);
@@ -7,10 +64,11 @@ static void TomlParser::setFile(string name)
 }
 static void TomlParser::load()
 {
-    bool formatStrMode = false, rawStrMode = false, escMode = false, endOfLine = false;
+    bool formatStrMode = false, rawStrMode = false, escMode = false, endOfStrMode = false; endOfLine = false;
     string currLine = "";
     string currStr = "";
     TomlHash currHash = null;
+    TomlElement currElement = null;
     int lineNumber = 1;
     char currChar;
     for(; !stream.eof(); lineNumber++)//this is the line loop
@@ -22,14 +80,15 @@ static void TomlParser::load()
             throw new TomlError("Multiline strings not supported. If you insist, use \"\\n\" instead.", lineNumber);
         }
         rawStrMode = false;
-        formatStrMode = false;;
+        formatStrMode = false;
         getline(stream, currLine);
+        clipSpaces(currLine);
         for(int i=1; i<currLine.length(); i++)//this goes over each individual character
         {
             currChar = currLine[i];
             if(endOfLine)
             {
-                throw new TomlError("What are you trying to do with these random characters?",lineNumber);
+                throw new TomlError("What are you trying to do with these random characters?", lineNumber);
             }
             else if(formatStrMode)
             {
@@ -40,6 +99,7 @@ static void TomlParser::load()
                 if(currChar=='\'')
                 {
                     rawStrMode = false;
+                    endOfLine = true;
                 }
                 else
                 {
@@ -69,10 +129,54 @@ static void TomlParser::load()
                         currHash = new TomlHash(hashName, currHash);
                         endOfLine = true;
                         break;
-                    default:
-                        //
-                }
-            }
-        }
-    }
+                    default://it's a key
+                        if(endOfStrMode)
+                        {
+                            currElement.type = "STRING";
+                            currElement.value = currStr;
+                            break;
+                        }
+                        string name = "";
+                        while(currChar != '=')
+                        {
+                            name.append(currChar);
+                            i++;
+                            currChar = currLine.at(i);
+                        }
+                        i++;//we go past the =
+                        currChar = currLine.at(i);
+                        valueStr = line.substr(i + 1);
+                        currElement = new TomlElement(name, "");
+                        try
+                        {
+                            long value = parseInteger(valueStr);
+                            currElement.value = valueStr;
+                            currElement.type = "INTEGER";
+                        }
+                        catch(int i)
+                        {
+                            try//This is the only way
+                            {
+                                bool value = parseBoolean(valueStr);
+                                currElement.value = valueStr;
+                                currElement.type = "BOOL";
+                                catch(int i)
+                                {
+                                    try
+                                    {
+                                        double value = parseDouble(line.substr(valueStr));
+                                        currElement.value = valueStr;
+                                        currElement.type = "DOUBLE";
+                                        catch(int i)
+                                        {
+                                            //now it's not a built-in type
+                                        }
+                                    }
+                                }
+                            }
+                        }//end try-catch
+                }//end switch
+            }//end else
+        }//end inner for
+    }//end outer for
 }
--- src/TomlParser.h
+++ src/TomlParser.h
@@ -18,6 +18,11 @@ class TomlParser
         static fstream stream;
         static vector<TomlElement> elements;
     public:
+        static long parseInteger(string text);
+        static bool parseBoolean(string text);
+        static double parseDouble(string text);
+        static void clipSpaces(string& text);
+        //static [Date] parseDate(string text);
         static void setFile(string name);
         static void load();//this is where everything's done
         static vector<TomlElement> getElements();
