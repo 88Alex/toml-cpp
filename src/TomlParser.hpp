8b0f0240e3c36ec16340fb2ffc03a92eacbbd5c5
Alex <epicpiano8@gmail.com>
2013-07-01 21:25:00 -0400

    Renamed *.h to *.hpp

---
+++ src/TomlParser.hpp
@@ -0,0 +1,31 @@
+#ifndef TOMLPARSER
+#define TOMLPARSER
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "TomlElement.h"
+#include "TomlKey.h"
+#include "TomlArray.h"
+#include "TomlHash.h"
+
+using namespace std;
+
+class TomlParser
+{
+    private:
+        fstream stream;
+        vector<TomlElement> elements;
+    public:
+        long parseInteger(string text);
+        bool parseBoolean(string text);
+        double parseDouble(string text);
+        void clipSpaces(string& text);
+        // [Date] parseDate(string text);
+        void setFile(string name);
+        void load();//this is where everything's done
+        vector<TomlElement> getElements();
+};
+
+#endif //TOMLPARSER
